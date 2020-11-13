29fe6da0d167951dd7dbab66c14ad2fe8ce37ab5
Keir Little <me@keirlittle.com>
2020-11-13 11:58:28 +0000

    FINAL: add call cost calculation and framework for testing

--- phone.py
+++ phone.py
@@ -1,8 +1,16 @@
-from datetime import datetime, timedelta
+import csv
+from collections import defaultdict
+from datetime import datetime, time, timedelta
+import json
+
+nightStart = time(hour=20)
+nightEnd = time(hour=8)
+
 
 class CallLog:
 
     def __init__(self, number, startTime, duration, direction):
+        """Assumes input is already sanitised and correctly-formatted"""
         self.number = self.normaliseNumber(number)
         self.startTime = datetime.fromisoformat(startTime)
         self.duration = self.parseDuration(duration)
@@ -53,3 +61,112 @@ class CallLog:
             return "mobile"
 
         return "invalid"
+
+
+def callCost(call, freeInternational, freeLocal):
+    """Calculates cost of a logged call, taking into account free minutes on
+    the tariff. Returns cost and remaining free minutes."""
+    callType = call.getCallType()
+    cost = 0
+
+    if call.direction == "INCOMING" or callType == "free" or callType == "invalid":
+        return cost, freeInternational, freeLocal
+
+    if callType == "international":
+        cost += 50  # connection charge
+
+        mins = startedMins(call.duration)
+        chargedMins = max(mins - freeInternational, 0)
+        remainingFree = max(freeInternational - mins, 0)
+
+        cost += chargedMins * 80
+        return cost, remainingFree, freeLocal
+
+    # All remaining calls must be landline or mobile. These need to be split
+    # into overnight and daytime minutes, but only considering non-free minutes
+    mins = startedMins(call.duration)
+    remainingFree = max(freeLocal - mins, 0)
+    if freeLocal > mins:
+        return cost, freeInternational, remainingFree
+
+    chargeStart = call.startTime + timedelta(minutes=remainingFree)
+    chargeEnd = call.startTime + call.duration
+    mins = startedMins(chargeEnd - chargeStart)
+
+    if chargeStart.time() > nightEnd and chargeEnd.time() < nightStart:
+        # Whole charged call is within daytime rate
+        if callType == "landline":
+            return 15 * mins, freeInternational, remainingFree
+        else:
+            return 30 * mins, freeInternational, remainingFree
+
+    if chargeStart.time() < nightEnd and chargeEnd.time() > nightStart:
+        # Whole charged call is within nighttime rate
+        if callType == "landline":
+            cost = 15 * mins
+        else:
+            cost = 30 * mins
+
+        # Spec doesn't specify how to deal with fractions of pennies,
+        # assuming floor instead of round
+        cost = cost//3
+        return cost, freeInternational, remainingFree
+
+    # Call time is split between day and night
+    dayMins = max(startedMins(nightEnd - chargeStart.time()), 0)
+    nightMins = max(startedMins(nightStart - chargeEnd.time()), 0)
+
+    if callType == "landline":
+        cost += 15 * dayMins
+        cost += (15 * nightMins)//3
+    else:
+        cost += 30 * dayMins
+        cost += (30 * nightMins)//3
+
+    return cost, freeInternational, remainingFree
+
+
+def startedMins(duration):
+    """Returns number of 'started' mins in a timedelta, which is always the
+    number of clock minutes plus 1, e.g. 00:59 is 1 minute, 01:00 is two"""
+    return duration / timedelta(mins=1) + 1
+
+
+def parseCallLog(callLogFilePath):
+    """Takes CSV of call log lines and returns list of CallLog objects"""
+    calls = []
+
+    with open(callLogFilePath) as csvFile:
+        for line in csv.reader(csvFile):
+            calls.append(CallLog(line[0], line[1], line[2], line[3]))
+
+    return calls
+
+
+def findMostExpensiveNumber(callLogFilePath):
+    # dict of {number: cost}, defaults to zero
+    cumulativeCosts = defaultdict(int)
+    freeInternational = 10
+    freeLocal = 100
+    callLog = parseCallLog(callLogFilePath)
+
+    for call in callLog:
+        cost, freeInternational, freeLocal = callCost(
+                call, freeInternational, freeLocal)
+
+        cumulativeCosts[call.number] += cost
+
+    maxCost = 0
+    maxNumber = ""
+
+    for number, cost in cumulativeCosts:
+        if cost > maxCost:
+            maxCost = cost
+            maxNumber = number
+
+    costString = "£{:}:{:}".format(maxCost//100, maxCost % 100)
+
+    return json.format({
+        "PhoneNumber": maxNumber,
+        "TotalAmount": costString,
+    })
--- test_phone.py
+++ test_phone.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 import pytest
 
-from .phone import CallLog
+from .phone import CallLog, callCost
+
 
 def test_normalise():
-    assert CallLog.normaliseNumber("01615556666") == CallLog.normaliseNumber("+441615556666") 
-    assert CallLog.normaliseNumber("01615556666") == CallLog.normaliseNumber("00441615556666") 
+    assert CallLog.normaliseNumber("01615556666") == CallLog.normaliseNumber("+441615556666")
+    assert CallLog.normaliseNumber("01615556666") == CallLog.normaliseNumber("00441615556666")
+    # ensure non-prefix "0044" isn't changed
+    assert CallLog.normaliseNumber("01615550044") == CallLog.normaliseNumber("00441615550044")
+
 
 @pytest.mark.parametrize("number,expected", [
     ["07624555666", "mobile"],
@@ -26,5 +30,14 @@ def test_callType(number, expected):
     startTime = datetime.now().isoformat()
     duration = "04:05"
     direction = "OUTGOING"
-    
+
     assert CallLog(number, startTime, duration, direction).getCallType() == expected
+
+
+@pytest.mark.parametrize("number,start,duration,direction,expected", [
+    ["07624555666", "1970-01-01T01:00:00", "10:00", "INCOMING", 0],  # INCOMING is free
+])
+def test_callCost(number, start, duration, direction, expected):
+    call = CallLog(number, start, duration, direction)
+
+    assert callCost(call, 0, 0) == (expected, 0, 0)
